46df8d4a052f95b64b60080fdb0da9fe379ace23
mohammedfahad <fahad1992.md@gmail.com>
2017-01-12 00:00:52 +1100

    initial push

---
+++ Template/controllers/api.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const async = require('async');
+const request = require('request');
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = (req, res) => {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+exports.getFileUpload = (req, res) => {
+  res.render('api/upload', {
+    title: 'File Upload'
+  });
+};
+
+exports.postFileUpload = (req, res) => {
+  req.flash('success', { msg: 'File was uploaded successfully.' });
+  res.redirect('/api/upload');
+};
+
+
+exports.getGoogleMaps = (req, res) => {
+  res.render('api/google-maps', {
+    title: 'Google Maps API'
+  });
+};
